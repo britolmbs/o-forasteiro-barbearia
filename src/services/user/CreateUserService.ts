bbb53c86549be04c46f1f143370c8289edf65cac
Lucas Brito <britolmbs@hotmail.com>
2025-08-04 18:33:28 -0300

    feat: Criando autenticação e usuario

--- src/services/user/CreateUserService.ts
+++ src/services/user/CreateUserService.ts
@@ -1,3 +1,6 @@
+import prismaClient from "../../prisma"
+import { hash } from "bcryptjs"
+
 interface UserRequest{
     name: string;
     email: string;
@@ -6,9 +9,36 @@ interface UserRequest{
 
 class CreateUserService {
     async execute({ name , email, password}: UserRequest){
-        console.log(name, email)
+       
+        if(!email){
+            throw new Error("Email incorreto");
+        }
 
-        return { ok: true }
+        const userAlreadyExists = await prismaClient.user.findFirst({
+            where:{
+                email: email
+            }
+        })
+
+        if(userAlreadyExists) {
+            throw new Error ("Usuario ou Email Já existe");
+        }
+
+        const passwordHash = await hash(password, 8)
+        const user = await prismaClient.user.create({
+            data:{
+                name: name,
+                email: email,
+                password: passwordHash
+            },
+            select:{
+                id: true,
+                name: true,
+                email: true,
+            }
+        })
+
+        return user;
     }
 }
 

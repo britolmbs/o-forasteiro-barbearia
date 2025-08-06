32112b642c21f6d22a1766de8b6223e49f079d03
Lucas Brito <britolmbs@hotmail.com>
2025-08-06 18:00:09 -0300

    feat: Criando o Middlewares, para autenticação de rotas

--- src/controllers/user/DetailUserController.ts
+++ src/controllers/user/DetailUserController.ts
@@ -4,6 +4,8 @@ import { UserDetailService } from '../../services/user/DetailUserService'
 class DetailUserController{
     async handle(request: Request, response: Response){
 
+        const user_id = request.user_id;
+
         const userDetailService = new UserDetailService();
 
         const datailUser = await userDetailService.execute();

96053e6ba0eb89df6663cf5405fd21f8600abac4
Lucas Brito <britolmbs@hotmail.com>
2025-08-08 09:27:42 -0300

    Feat: Criando Service e Controller para criação de corte

---
+++ src/services/haircut/CreateHaircutService.ts
@@ -0,0 +1,45 @@
+import prismaClient from "../../prisma";
+
+interface HaircutRequest{
+    user_id: string;
+    name: string;
+    price: number;
+}
+
+class CreateHaircutService{
+    async execute({user_id, name, price}: HaircutRequest){
+        if(!name || !price){
+            throw new Error("Necessario Nome e Preço")
+        }
+
+        const myHaircuts = await prismaClient.haircut.count({
+            where:{
+                user_id: user_id
+            }
+        })
+
+        const user = await prismaClient.user.findFirst({
+            where:{
+                id: user_id,
+            },
+            include:{
+                subscriptions: true,
+            }
+        })
+
+        if(myHaircuts >= 3 && user?.subscriptions?.status !== 'active'){
+            throw new Error("Não Atorizado")
+        }
+
+        const haircut = await prismaClient.haircut.create({
+            data:{
+                name: name,
+                price: price,
+                user_id: user_id
+            }
+        })
+        return haircut;
+    }
+}
+
+export { CreateHaircutService }

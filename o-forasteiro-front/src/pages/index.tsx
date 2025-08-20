c88f465f70cfa5aeeff50fb7c2b13c9f52b55b89
Lucas Brito <britolmbs@hotmail.com>
2025-08-20 19:58:31 -0300

    Feat: Criação do document

--- o-forasteiro-front/src/pages/index.tsx
+++ o-forasteiro-front/src/pages/index.tsx
@@ -1,7 +1,13 @@
+import Head from "next/head"
 export default function Home(){
     return (
         <>
+        <Head>
+            <title>O Forasteiro Barbearia - Seu Sistema Completo</title>
+        </Head>
+        <div>
         <h1>O Forasteiro</h1>
+        </div>
         </>
     )
 }

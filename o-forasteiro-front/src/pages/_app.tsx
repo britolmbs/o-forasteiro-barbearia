35abeae8ea0a19db409fed197c7f0f692bbaaf42
Lucas Brito <britolmbs@hotmail.com>
2025-08-21 17:19:48 -0300

    adicionando o chakra

--- o-forasteiro-front/src/pages/_app.tsx
+++ o-forasteiro-front/src/pages/_app.tsx
@@ -1,7 +1,13 @@
 import type { AppProps } from "next/app"
+import { Provider } from "@/components/ui/provider"
+import  from '@chakra-ui/react'
 
 function MyApp({ Component, pageProps}: AppProps) {
-    return <Component {...pageProps} />
+    return (
+    <Provider>
+        <Component {...pageProps} />
+    </Provider>
+    )
 }
 
 export default MyApp

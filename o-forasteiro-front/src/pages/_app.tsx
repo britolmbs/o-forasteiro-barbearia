252da4dc146082c2625fd03c6f53e5cb8e90ebe8
Lucas Brito <britolmbs@hotmail.com>
2025-08-22 09:10:09 -0300

    Feat: configuração e criação do theme com chakra Ui

--- o-forasteiro-front/src/pages/_app.tsx
+++ o-forasteiro-front/src/pages/_app.tsx
@@ -1,13 +1,35 @@
 import type { AppProps } from "next/app"
-import { Provider } from "@/components/ui/provider"
-import  from '@chakra-ui/react'
+import { ChakraProvider, createSystem, defaultConfig } from "@chakra-ui/react"
 
-function MyApp({ Component, pageProps}: AppProps) {
-    return (
-    <Provider>
-        <Component {...pageProps} />
-    </Provider>
-    )
+const system = createSystem(defaultConfig, {
+  theme: {
+    tokens: {
+      colors: {
+        barber: {
+          900: { value: '#12131b' },
+          400: { value: '#1b1c29' },
+          100: { value: '#c6c6c6' }
+        },
+        button: {
+          cta: { value: '#fba931' },
+          default: { value: '#FFF' },
+          gray: { value: '#DFDFDF' },
+          danger: { value: '#FF4040' },
+        },
+        orange: {
+          900: { value: '#fba931' }
+        }
+      }
+    }
+  }
+})
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider value={system}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
--- o-forasteiro-front/src/pages/index.tsx
+++ o-forasteiro-front/src/pages/index.tsx
@@ -1,13 +1,15 @@
 import Head from "next/head"
+import {Flex, Text } from '@chakra-ui/react'
+
 export default function Home(){
     return (
         <>
         <Head>
             <title>O Forasteiro Barbearia - Seu Sistema Completo</title>
         </Head>
-        <div>
-        <h1>O Forasteiro</h1>
-        </div>
+        <Flex background="barber.900" height="100vh" alignItems='center' justifyContent='center'>
+        <Text fontSize={30}>O Forasteiro</Text>
+        </Flex>
         </>
     )
 }

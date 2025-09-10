61d55de20c91b06065d1c2024298fd53ca8c5c5e
Lucas Brito <britolmbs@hotmail.com>
2025-09-10 17:31:37 -0300

    Feat: Criando pagina do Profile

---
+++ o-forasteiro-front/src/pages/profile/index.tsx
@@ -0,0 +1,22 @@
+import { Siderbar } from "@/components/sidebar";
+import { Flex } from "@chakra-ui/react";
+import Head from "next/head";
+
+export default function Profile(){
+    return(
+        <>
+        <Head>
+            <title>
+                Minha Conta - O Forasteiro
+            </title>
+        </Head>
+        <Siderbar>
+            <Flex>
+                <Flex>
+                    
+                </Flex>
+            </Flex>
+        </Siderbar>
+        </>
+    )
+}

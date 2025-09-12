0c6f4f18541931b7717ca4f1b010de6c13f61a06
Lucas Brito <britolmbs@hotmail.com>
2025-09-12 17:07:53 -0300

    Feat: Pegando o estado do BackEnd

--- o-forasteiro-front/src/pages/profile/index.tsx
+++ o-forasteiro-front/src/pages/profile/index.tsx
@@ -3,12 +3,28 @@ import { canSSRAuth } from "@/utils/canSSRAuth";
 import { Box, Button, Flex, Heading, Input, Text } from "@chakra-ui/react";
 import Head from "next/head";
 import Link from "next/link";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "@/context/AuthContext";
+import { setupAPIClient } from "@/services/api";
 
-export default function Profile(){
+interface UserProps{
+    id: string;
+    name: string;
+    email: string;
+    endereco: string | null
+}
+
+interface ProfileProps{
+    user: UserProps;
+    premium: boolean;
+}
+
+export default function Profile({user, premium}: ProfileProps ){
     const {logoutUser} = useContext(AuthContext);
-    
+
+    const [name, setName] = useState(user && user?.name);
+    const [endereco, setEndereco] = useState(user && user?.endereco);
+
     async function handleLogout() {
         await logoutUser();
     }
@@ -34,6 +50,8 @@ export default function Profile(){
                     placeholder="Nome da Barbearia"
                     size='lg'
                     mb={3}
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
                     />
                     <Text mb={2} fontSize='xl' fontWeight='bold' color='white'>Endereço:</Text>
                     <Input w='100%'
@@ -41,13 +59,15 @@ export default function Profile(){
                     placeholder="Endereço da barbearia"
                     size='lg'
                     mb={3}
+                    value={endereco}
+                    onChange={(e) => setEndereco(e.target.value)}
                     />
                     <Text mb={3} fontSize='xl' fontWeight='bold' color='white'>Plano Atual</Text>
                     <Flex direction='row' w='100%' mb={3} p={1} borderWidth={1} rounded={6} background='barber.900'
                     alignItems='center' justifyContent='space-between' 
                     >
                         
-                        <Text p={2} fontSize='lg' color="#4dffb4">Plano Gratis</Text>
+                        <Text p={2} fontSize='lg' color={premium ? "#FBA931" : "#4dffb4"}>Plano {premium ? 'Premium' : 'Grátis' }</Text>
                         <Link href='/planos'>
                         <Box cursor='pointer' p={1} pl={2} pr={2} background="#00cd52" rounded={4} color='white'>Mudar Plano</Box>
                         </Link>
@@ -66,7 +86,33 @@ export default function Profile(){
 }
 
 export const getServerSideProps = canSSRAuth(async (ctx) =>{
-    return {
-        props:{}
+
+    try {
+        const apiClient = setupAPIClient(ctx)
+        const response = await apiClient.get('/me')
+
+        const user = {
+            id: response.data.id,
+            name: response.data.name,
+            email: response.data.email,
+            endereco: response.data?.endereco,
+        }
+
+        return {
+            props:{
+                user: user,
+                premium: response.data?.subscriptions?.status === 'active'  ? true : false
+            }
+        }
+    } catch (err) {
+        console.log(err);
+        return{
+            redirect:{
+                destination:'/dashboard',
+                permanent: false,
+            }
+        
+
+        }
     }
 })
